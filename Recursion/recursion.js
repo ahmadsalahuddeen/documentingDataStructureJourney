5623f5d2480dea5a3a693f1fbadca1b191082088
ahmadsalahuddeen <ahmadsalahuddeen6017@gmail.com>
2023-04-18 20:01:22 +0530

    factorial in common way

--- Recursion/recursion.js
+++ Recursion/recursion.js
@@ -1,18 +1,33 @@
 // a recersive function to countdown and log the number
+const countDown = (number) => {
+  if (number <= 0) {
+    console.log("All is done");
+    return;
+  }
+  console.log(number);
+  number--;
+  countDown(number);
+};
+
+//countDown(10); <--------------------  invoking the function
 
 
-const countDown = (number) =>{
-    if (number <=  0) {
-        console.log('All is done')
-        return;
-    }    
-    console.log(number);
-    number--;
-    countDown(number)    
-   
 
-    
 
+
+
+// factorial in common way
+function commonFactorial(number) {
+  // outpul should be a total  number
+  //loop start from input till 2
+  total = 1;
+
+  for (i = number; i >= 2; i--) {
+    total *= i;
+  }
+  console.log(total);
 }
+commonFactorial(5);
 
-countDown(10)
+
+
