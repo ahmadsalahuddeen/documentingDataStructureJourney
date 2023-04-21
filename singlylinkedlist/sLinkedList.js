c58aaa48bb44bdea8f7d6565a8f9f58af83a132a
ahmadsalahuddeen <ahmadsalahuddeen6017@gmail.com>
2023-04-21 11:20:22 +0530

    new node and push val to the list

---
+++ singlylinkedlist/sLinkedList.js
@@ -0,0 +1,33 @@
+class Node {
+    constructor(val){
+        this.val  = val;
+        this.next = null;
+    }
+}
+
+class sLinkedList {
+    //constructor is a special function that invoke at the time of initialization of the object of this class 
+    constructor(){
+this.head = null;
+this.length = 0;
+this.tail  = null
+    }
+    push(value) {
+        let newNode = new Node(value)
+        if(!this.head ){
+            this.head = newNode;
+            this.tail = this.head
+
+        }else{
+             this.tail.next = newNode;
+             this.tail = newNode
+        }
+        this.length++
+
+        return this;
+    } 
+
+    }
+
+var list = new sLinkedList()
+ list.push('hi')
