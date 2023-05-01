1a57b3ac068c1af6ecd9b0c916945a0d7f104016
ahmadsalahuddeen <ahmadsalahuddeen6017@gmail.com>
2023-05-01 07:28:34 +0530

    stack-push

--- stack/stack.js
+++ stack/stack.js
@@ -6,5 +6,29 @@ this.next = null
 }
 
 
+class Stack {
+
+    constructor(){
+        this.start = null
+        this.end = null
+        this.length = 0
+    }
+
+    push(value){
+        newNode = new Node(value)
+if(!this.start){
+this.start = newNode
+this.end = newNode
+
+    }else{
+        temp = this.start
+        this.start = newNode
+        this.start.next = temp
+
+    }
+    return ++this.length
+}
 
 
+
+}
