e1d0fbeea65f52c9fcbe4e4c882b611e48e05b5b
ahmadsalahuddeen <ahmadsalahuddeen6017@gmail.com>
2023-05-05 07:33:14 +0530

    .

--- binarySearchTree/BSTree.js
+++ binarySearchTree/BSTree.js
@@ -1,25 +1,43 @@
 class Node {
-    constructor(value){
-        this.value = value
-        this.right  = null
-        this.left = null    
-    }
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
+  }
 }
 
 class BinarySearchTree {
-    constructor(){
-        this.root = null
-    }
+  constructor() {
+    this.root = null;
+  }
 
-    insert(value){
-    let newNode = new Node(value)
-        while(true){
-            if(this.root === null){
-                this.root = newNode;
+  insert(value) {
+    let newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if(value < currentNode.value){
+            if(currentNode.left === null){
+                currentNode.left = newNode;
                 return this;
+            }else{
+                currentNode = currentNode.left
             }
-}
+
+        }else if(value > currentNode.value){
+            if(currentNode.right == null){
+                currentNode.right = newNode
+                return this;
+            }else{
+                currentNode = currentNode.right;
+                
+            }
+        }
+      }
     }
-
-
+  }
 }
