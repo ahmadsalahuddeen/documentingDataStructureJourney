1bffa1fd59a9fc3475d08776a5dda2cd15a42488
ahmadsalahuddeen <ahmadsalahuddeen6017@gmail.com>
2023-05-05 08:08:27 +0530

    tree search

--- binarySearchTree/BSTree.js
+++ binarySearchTree/BSTree.js
@@ -20,42 +20,60 @@ class BinarySearchTree {
     } else {
       let currentNode = this.root;
       while (true) {
-        if(value === currentNode.value) return undefined;
-        if(value < currentNode.value){
-            if(currentNode.left === null){
-                currentNode.left = newNode;
-                console.log(JSON.stringify(this, null, 4));
+        if (value === currentNode.value) return undefined;
+        if (value < currentNode.value) {
+          if (currentNode.left === null) {
+            currentNode.left = newNode;
+            console.log(JSON.stringify(this, null, 4));
 
-                
-                return this;
-            }else{
-                currentNode = currentNode.left
-            }
+            return this;
+          } else {
+            currentNode = currentNode.left;
+          }
+        } else if (value > currentNode.value) {
+          if (currentNode.right == null) {
+            currentNode.right = newNode;
+            console.log(JSON.stringify(this, null, 4));
 
-        }else if(value > currentNode.value){
-            if(currentNode.right == null){
-                currentNode.right = newNode
-                console.log(JSON.stringify(this, null, 4));
-
-
-                return this;
-            }else{
-                currentNode = currentNode.right;
-
-            }
+            return this;
+          } else {
+            currentNode = currentNode.right;
+          }
         }
       }
     }
   }
+  find(value){
+if(this.root === null) return console.log('root is null')
+let currentNode = this.root
+let found = false
+while(!found && currentNode){
+if(value < currentNode.value){
+    currentNode = currentNode.left;
+
+}else if(value > currentNode.value){
+    currentNode = currentNode.right
+}else{
+    found = true
+    return console.log('found:'+ currentNode.value )
 }
 
+}
+return console.log('cant find the value')
 
-let tree = new BinarySearchTree()
+  }
+}
 
-tree.insert(34)
-tree.insert(98)
-tree.insert(1)
-tree.insert(23)
-tree.insert(45)
-tree.insert(5)
-tree.insert(1234)
+let tree = new BinarySearchTree();
+
+tree.insert(34);
+tree.insert(98);
+tree.insert(1);
+tree.insert(23);
+tree.insert(45);
+tree.insert(45);
+tree.insert(5);
+tree.insert(1234);
+
+
+tree.find(5)
