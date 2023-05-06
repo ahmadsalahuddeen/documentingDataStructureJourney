34674fe1ce46b7a02f924d2ab5a9c27e32bafb58
ahmadsalahuddeen <ahmadsalahuddeen6017@gmail.com>
2023-05-06 14:50:25 +0530

    max heap

--- Heap/maxBinaryHeap.js
+++ Heap/maxBinaryHeap.js
@@ -1,15 +1,31 @@
 class Heap {
 
     constructor(){
-        this.value = []
+        this.value = [41, 39, 33, 18, 27, 12]
 
     }
 
     insert(newVal){
         this.value.push(newVal) 
         this.bubbleup(newVal)
+        console.log(this.value)
     }
     bubbleup(valueToInsert){
-         
+         let idx = this.value.length - 1
+         while(idx > 0){
+let parentIdx =  Math.floor( (idx - 1)/2)
+let parentValue = this.value[parentIdx]
+if(valueToInsert <= parentValue) break;
+    this.value[parentIdx] = valueToInsert
+    this.value[idx] = parentValue
+    idx = parentIdx
+
+
+         }
     }
 }
+
+let heap = new Heap()
+
+
+heap.insert(55)
