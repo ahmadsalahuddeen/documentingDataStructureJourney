51972f8e12a54d21b0cedfd16ccb2e1b329d181a
ahmadsalahuddeen <ahmadsalahuddeen6017@gmail.com>
2023-05-09 18:22:38 +0530

    min heap

---
+++ Heap/minBinaryHeap.js
@@ -0,0 +1,22 @@
+class Heap {
+    constructor(){
+        this.values = []
+    }
+
+    insert(value){
+this.values.push(value)
+this.sort(value)
+    }
+    sort(value){
+let numberToInsert = value
+    currentIndex = this.values.length -1
+    while(currentIndex > 0){
+        let parentIndex = Math.floor((currentIndex -1)/2)
+        if(values[parentIndex] < values[currentIndex] ){
+
+        }
+    }
+
+
+    }
+}
