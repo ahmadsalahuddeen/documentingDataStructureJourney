8b44e58a746e7d2c154e74e2e3ede47c34cf775c
ahmadsalahuddeen <ahmadsalahuddeen6017@gmail.com>
2023-05-11 16:05:27 +0530

    .

--- sorting/quickSort/quickSort.js
+++ sorting/quickSort/quickSort.js
@@ -7,11 +7,26 @@ function pivot(arr, start=0, end=arr.length){
             [arr[swapindex], arr[i]] = [arr[i], arr[swapindex]]
         }
     }
-    [arr[start], arr[swapindex]] = [arr[i], arr[swapindex]]
+    [arr[start], arr[swapindex]] = [arr[swapindex], arr[start]]
 return swapindex
 }
 
 
-function quickSort (arr, left=0, right= arr.length){
+function quickSort (arr, left = 0, right = arr.length-1){
+
     
+    if(left < right){
+        let pivotIndex = pivot(arr, left, right)
+    quickSort(arr, left, pivotIndex - 1 )
+    quickSort(arr, pivotIndex + 1, right)
+    }
+    return arr
+
+
+
 }
+
+
+ 
+let result = quickSort([23,465,1,5,3,6,4,33,72])
+console.log(result)
