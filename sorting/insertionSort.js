d176051ff248c6e9d536ac267b707ea4de50fef7
ahmadsalahuddeen <ahmadsalahuddeen6017@gmail.com>
2023-05-11 12:17:54 +0530

    .

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -1,15 +1,12 @@
-function sort(arr){
-for(i = 1; i< arr.length; i++){
-let numberToInsert = arr[i]
-for(j= i-1; j<= 0 && arr[j]> numberToInsert; j--){
-
-arr[j+1] = arr[j]
-
-}
-arr[j+1] = numberToInsert
-}
-console.log(arr)
+function sort(arr) {
+  for (i = 1; i < arr.length; i++) {
+    let numberToInsert = arr[i];
+    for (j = i - 1; j <= 0 && arr[j] > numberToInsert; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = numberToInsert;
+  }
+  console.log(arr);
 }
 
-
-sort([23,4,2,6,7,2,7,94,53,12,86])
+sort([23, 4, 2, 6, 7, 2, 7, 94, 53, 12, 86]);
--- sorting/quickSort/quickSort.js
+++ sorting/quickSort/quickSort.js
@@ -1,30 +1,14 @@
 function pivot(arr, start=0, end=arr.length){
-let pivot = arr[start]
-let swapIndex = start
-
-for(i = start+1; i< arr.length; i++){
-    if(pivot> arr[i]){
-        swapIndex++
-        [arr[swapIndex], arr[i]] = [arr[i], arr[swapIndex]]
+    let swapindex = start
+    let pivot = arr[start]
+    for(i= start+ 1 ; i< arr.length; i++){
+        if(pivot > arr[i]){
+            swapindex++
+            [arr[swapindex], arr[i]] = [arr[i], arr[swapindex]]
+        }
     }
-}
-[arr[swapIndex], arr[start]] = [arr[start], arr[swapIndex]]
-return swapIndex;
-
-
+    [arr[start], arr[swapindex]] = [arr[i], arr[swapindex]]
 }
 
 
-
-
-function quickSort( arr, left= 0, right = arr.length-1){
-    if (left < right) {
-        let pivotIndex = pivot(arr, left, right)
-        quickSort(arr, left, pivotIndex-1)
-        quickSort(arr,  pivotIndex+1, right)
-        
-    }
-    return arr;
-}
-
-console.log( quickSort([23, 56,2,1,5,3,32,11, 4,11123,34]))
+function quickSort = 
