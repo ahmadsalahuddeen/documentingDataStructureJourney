99ece8be5268a6cb66a2c6b2577ffd8e05d15cba
ahmadsalahuddeen <ahmadsalahuddeen6017@gmail.com>
2023-05-15 22:58:44 +0530

    remove from heap

--- Heap/maxBinaryHeap.js
+++ Heap/maxBinaryHeap.js
@@ -1,31 +1,47 @@
 class Heap {
+  constructor() {
+    this.value = [41, 39, 33, 18, 27, 12];
+  }
 
-    constructor(){
-        this.value = [41, 39, 33, 18, 27, 12]
-
+  insert(newVal) {
+    this.value.push(newVal);
+    this.bubbleup(newVal);
+    console.log(this.value);
+  }
+  bubbleup(valueToInsert) {
+    let idx = this.value.length - 1;
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parentValue = this.value[parentIdx];
+      if (valueToInsert <= parentValue) break;
+      this.value[parentIdx] = valueToInsert;
+      this.value[idx] = parentValue;
+      idx = parentIdx;
     }
+  }
 
-    insert(newVal){
-        this.value.push(newVal) 
-        this.bubbleup(newVal)
-        console.log(this.value)
+  //extract max/ delete
+
+  deleteMax() {
+    let idx = 0;
+    let element = this.values.pop();
+    this.value[0] = element;
+    while (true) {
+      let length = this.value.length - 1;
+      let leftChildIndex = 2 * idx + 1;
+      let rightChildIndex = 2 * idx + 2;
+      let leftValue, rightValue;
+      let swap = null;
+      if (leftChildIndex < length) {
+        leftValue = this.value[leftChildIndex];
+        if (leftValue > element) {
+          swap = leftChildIndex;
+        }
+      }
     }
-    bubbleup(valueToInsert){
-         let idx = this.value.length - 1
-         while(idx > 0){
-let parentIdx =  Math.floor( (idx - 1)/2)
-let parentValue = this.value[parentIdx]
-if(valueToInsert <= parentValue) break;
-    this.value[parentIdx] = valueToInsert
-    this.value[idx] = parentValue
-    idx = parentIdx
-
-
-         }
-    }
+  }
 }
 
-let heap = new Heap()
+let heap = new Heap();
 
-
-heap.insert(55)
+heap.insert(55);
